84224345ffb0d2df7639a0340d7a98841f024787
Igor <igorcpf@gmail.com>
2021-03-03 23:10:32 -0300

    WIP organizing user info layout grid

--- app/src/pages/userInfo/UserInfo.tsx
+++ app/src/pages/userInfo/UserInfo.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { User } from "../../services/types";
-import { Wrapper } from "./styles";
-import { useLocation } from "react-router-dom";
-
+import { Wrapper, PersonalInfo, ReposInfo, Topbar, BackButton } from "./styles";
+import { useLocation, useHistory } from "react-router-dom";
+import { IoArrowBackCircleOutline } from "react-icons/io5";
 const UserInfo = () => {
 	const { state: user } = useLocation<User>();
+	const history = useHistory();
 	console.log("User", user);
 
-	return <Wrapper></Wrapper>;
+	return (
+		<Wrapper>
+			<Topbar>
+				<BackButton onClick={() => history.goBack()}>
+					<IoArrowBackCircleOutline />
+				</BackButton>
+			</Topbar>
+			<PersonalInfo></PersonalInfo>
+			<ReposInfo></ReposInfo>
+		</Wrapper>
+	);
 };
 
 export default UserInfo;
--- app/src/pages/userInfo/styles.ts
+++ app/src/pages/userInfo/styles.ts
@@ -4,4 +4,38 @@ export const Wrapper = styled.div`
 	height: 100%;
 	width: 100%;
 	background-color: #f1f1f1;
+	display: grid;
+	grid-template-columns: 2fr 3fr;
+	grid-template-rows: 0.5fr 9.5fr;
+	grid-template-areas:
+		"topbar topbar"
+		"personInfo reposInfo";
+
+	@media (max-width: 768px) {
+		grid-template-columns: 1fr;
+		grid-template-rows: 0.5fr 3.5fr 6fr;
+		grid-template-areas: "topbar" "personInfo" "reposInfo";
+	}
+`;
+
+export const Topbar = styled.div`
+	grid-area: topbar;
+	display: flex;
+`;
+
+export const BackButton = styled.button`
+	outline: none;
+	border: none;
+	cursor: pointer;
+	font-size: 2rem;
+`;
+
+export const PersonalInfo = styled.div`
+	grid-area: personInfo;
+	border: 1px solid green;
+`;
+
+export const ReposInfo = styled.div`
+	grid-area: reposInfo;
+	border: 1px solid tomato;
 `;
