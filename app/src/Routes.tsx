7f515a7099078ab009db7cd292f67a4d7526ceaf
Igor <igorcpf@gmail.com>
2021-03-10 18:00:22 -0300

    minor fix

--- app/src/Routes.tsx
+++ app/src/Routes.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route, MemoryRouter } from "react-router-dom";
 import Search from "./pages/search/Search";
 import UserInfo from "./pages/userInfo/UserInfo";
 import SearchHistory from "./pages/searchHistory/SearchHistory";
 
 const Routes = () => {
 	return (
-		<BrowserRouter>
+		<MemoryRouter>
 			<Route component={Search} exact path="/" />
 			<Route component={UserInfo} path="/userInfo" />
 			<Route component={SearchHistory} path="/search-history" />
-		</BrowserRouter>
+		</MemoryRouter>
 	);
 };
 
